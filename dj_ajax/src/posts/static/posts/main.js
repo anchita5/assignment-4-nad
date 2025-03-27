1b69c6010829de7c08b118e0b9eb1d68a77d1e5a
Anchita Kakria <Akakria1585@conestogac.on.ca>
2025-03-27 02:57:48 -0400

    getting posts object with ajax

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -1,6 +1,7 @@
 console.log('hello world')
 
 const helloWorldBox = document.getElementById('hello-world')
+const postsBox = document.getElementById('posts-box')
 
 
 $.ajax({
@@ -15,3 +16,22 @@ $.ajax({
     }
 
 })
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response) {
+        console.log(response);
+        const data = response.data;
+        console.log(data);
+
+        data.forEach(el => {
+            postsBox.innerHTML += `
+                ${el.title} - <b>${el.body}</b><br>
+            `;
+        });
+    },
+    error: function(error) {
+        console.log(error);
+    }
+});
