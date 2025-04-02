ee5e61c3035c30179d14a4989caa5c141091dd35
Anchita Kakria <Akakria1585@conestogac.on.ca>
2025-04-02 06:25:33 -0400

    added a new button to load more posts but i have minor errors to fix yet

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -1,53 +1,50 @@
 console.log('hello world')
 
-const helloWorldBox = document.getElementById('hello-world')
 const postsBox = document.getElementById('posts-box')
-const spinnerBox = document.getElementById('spinner-box')
+const loadBtn= document.getElementById('load-btn')
 
-$.ajax({
-    type: 'GET',
-    url: '/hello-world/',
-    success: function(response){
-        console.log('success', response.text)
-        helloWorldBox.textContent = response.text
-    },
-    error: function(error){
-        console.log('error', error)
-    }
+let visible = 3
 
-})
+const getData = () => {
+    $.ajax({
+        type: 'GET',
+        url: `/data/${visible}/`,
+        success: function(response) {
+            console.log(response)
+            const data = response.data
+            setTimeout(() => {    
+                console.log(data)
+                data.forEach(el => {
+                    postsBox.innerHTML += `
+                        <div class="card mb-2">
+                             <div class="card-body">
+                             <h5 class="card-title">${el.title}</h5>
+                             <p class="card-text">${el.body}</p>
+                             </div>
+                             <div class="card-footer">
+                             <div class="row">
+                             <div class="col-1">
+                             <a href="#" class="btn btn-primary">Details</a>
+                             </div>
+                             <div class="col-1">
+                             <a href="#" class="btn btn-primary">Like</a>
+                             </div>
+                             </div>
+                             </div>
+                        </div>
+                    `;
+                });
+            },0);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    })
+}
 
-$.ajax({
-    type: 'GET',
-    url: '/data/',
-    success: function(response) {
-        console.log(response)
-        const data = response.data
-        setTimeout(() => {    
-            console.log(data)
-            data.forEach(el => {
-                postsBox.innerHTML += `
-                    <div class="card mb-2"">
-                         <div class="card-body">
-                         <h5 class="card-title">${el.title}</h5>
-                         <p class="card-text">${el.body}</p>
-                         </div>
-                         <div class="card-footer">
-                         <div class="row">
-                         <div class="col-1">
-                         <a href="#" class="btn btn-primary">Details</a>
-                         </div>
-                         <div class="col-1">
-                         <a href="#" class="btn btn-primary">Like</a>
-                         </div>
-                         </div>
-                         </div>
-                    </div>
-                `;
-            });
-        })
-    },
-    error: function(error) {
-        console.log(error);
-    }
+loadBtn.addEventListener('click', ()=>{
+    visible +=3
+    getData()
 });
+
+getData()
