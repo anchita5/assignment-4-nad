14345aaedf1cb3665d7c187e4a023747283012ee
Anchita Kakria <Akakria1585@conestogac.on.ca>
2025-04-05 01:37:48 -0400

    added form reset feature and to det details of the post page

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -6,10 +6,11 @@ const postForm=document.getElementById('post-form')
 const title=document.getElementById('id_title')
 const body=document.getElementById('id_body')
 const csrf=document.getElementsByName('csrfmiddlewaretoken')
+
+const url = window.location.href
+
 const alertBox=document.getElementById('alert-box')
-
 console.log('csrf',csrf[0].value)
-
 
 const getCookie = (name) => {
     let cookieValue = null;
@@ -77,7 +78,7 @@ const getData = () => {
                              <div class="card-footer">
                              <div class="row">
                              <div class="col-2">
-                             <a href="#" class="btn btn-primary">Details</a>
+                             <a href="${url}${el.id}" class="btn btn-primary">Details</a>
                              </div>
                              <div class="col-2">
                                 <form class="like-unlike-forms" data-form-id="${el.id}">
@@ -146,6 +147,7 @@ postForm.addEventListener('submit', e=>{
                 likeUnlikePosts()
                 $('#addPostModal').modal('hide')
                 handleAlerts('success','New Posts added!!')
+                postForm.reset()
         },
         error:function(error){
             console.log(error)
