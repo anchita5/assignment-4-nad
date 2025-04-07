0aa3710227d4c945bb738d1a4c0305eec32362e5
Anchita Kakria <Akakria1585@conestogac.on.ca>
2025-04-07 00:31:13 -0400

    worked on dropzone with posts, also added some limitations

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -10,7 +10,10 @@ const csrf=document.getElementsByName('csrfmiddlewaretoken')
 const url = window.location.href
 
 const alertBox=document.getElementById('alert-box')
-console.log('csrf',csrf[0].value)
+
+const dropzone = document.getElementById('my-dropzone')
+const addBtn = document.getElementById('add-btn')
+const closeBtns = [...document.getElementsByClassName('add-modal-close')]
 
 const getCookie = (name) => {
     let cookieValue = null;
@@ -88,7 +91,7 @@ const getData = () => {
                              </div>
                              <div class="col-2">
                                 <form class="like-unlike-forms" data-form-id="${el.id}">
-                                     <button href="#" class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike(${el.count})`:`Like (${el.count})`}</button>
+                                     <button  class="btn btn-primary" id="like-unlike-${el.id}">${el.liked ? `Unlike(${el.count})`:`Like (${el.count})`}</button>
                                 </form>
                              </div>
                              </div>
@@ -118,6 +121,7 @@ loadBtn.addEventListener('click', ()=>{
     getData()
 })
 
+let newPostId=null
 postForm.addEventListener('submit', e=>{
     e.preventDefault()
     $.ajax({
@@ -130,6 +134,7 @@ postForm.addEventListener('submit', e=>{
         },
         success:function(response){
             console.log(response)
+            newPostId= response.id
             postsBox.insertAdjacentHTML('afterbegin',`
                 <div class="card mb-2">
                              <div class="card-body">
@@ -139,11 +144,11 @@ postForm.addEventListener('submit', e=>{
                              <div class="card-footer">
                              <div class="row">
                              <div class="col-2">
-                             <a href="#" class="btn btn-primary">Details</a>
+                             <a href="${url}${response.id}" class="btn btn-primary">Details</a>
                              </div>
                              <div class="col-2">
                                 <form class="like-unlike-forms" data-form-id="${response.id}">
-                                     <button href="#" class="btn btn-primary" id="like-unlike-${response.id}">Like (0)</button>
+                                     <button class="btn btn-primary" id="like-unlike-${response.id}">Like (0)</button>
                                 </form>
                              </div>
                              </div>
@@ -151,9 +156,9 @@ postForm.addEventListener('submit', e=>{
                         </div>
                 `)
                 likeUnlikePosts()
-                $('#addPostModal').modal('hide')
+                //$('#addPostModal').modal('hide')
                 handleAlerts('success','New Posts added!!')
-                postForm.reset()
+                //postForm.reset()
         },
         error:function(error){
             console.log(error)
@@ -161,5 +166,32 @@ postForm.addEventListener('submit', e=>{
         }
     })
 })
+
+addBtn.addEventListener('click',()=>{
+    dropzone.classList.remove('not-visible')
+})
+closeBtns.forEach(btn=> btn.addEventListener('click',()=>{
+    postForm.reset()
+    if(!dropzone.classList.contains('not-visible')){
+        dropzone.classList.add('not-visible')
+    }
+    const myDropzone= Dropzone.forElement("#my-dropzone")
+    myDropzone.removeAllFiles(true)
+}))
+
+Dropzone.autoDiscover = false
+const myDropzone = new Dropzone('#my-dropzone',{
+    url: 'upload/',
+    init: function(){
+        this.on('sending', function(file, xhr, formData){
+            formData.append('csrfmiddlewaretoken', csrftoken)
+            formData.append('new_post_id', newPostId)
+        })
+    },
+    maxFiles: 3,
+    maxFilesize:4,
+    acceptedFiles:'.png, .jpg, .jpeg'
+})
+
 
 getData()
